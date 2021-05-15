27e248277830bf005715b5ba5689a01e9ad94db4
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2021-05-14 19:09:13 -0500

    Create virtual environment

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/MCMi460/Ongaku
 cd Ongaku
-python3 -m pip install -r requirements.txt py2app
-python3 setup.py py2app
+python3 -m venv vpy
+source vpy/bin/activate
+python -m pip install -r requirements.txt py2app
+python setup.py py2app
 open ./dist
 rm ../build.sh
