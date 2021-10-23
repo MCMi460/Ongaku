78a4f075955964ddd94283fd5a9cd9107e979d64
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2021-10-23 17:47:37 -0500

    Quality of life improvements

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ curl https://api.github.com/repos/MCMi460/Ongaku/zipball/main -o Ongaku.zip -L
 unzip Ongaku.zip -d ./Ongaku
 rm Ongaku.zip
 cd ./Ongaku/*/
-python3 -m venv vpy
-source vpy/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 python -m pip install -r requirements.txt py2app
 python setup.py py2app
 open ./dist
